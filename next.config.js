edb2939da5b561a17f656cd2b2d6d855992f1b0f
sandoramix <sandoramixer61@gmail.com>
2022-07-09 09:34:26 +0200

    fixed leaflet access_token not avaiable

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: true,
-
+    env: {
+        TILES_TOKEN: process.env.TILES_TOKEN,
+    }
 }
 
 module.exports = nextConfig
