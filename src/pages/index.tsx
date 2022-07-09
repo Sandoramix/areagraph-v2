ffba0c8b4470b908b914b040f9f97dd031e39fce
sandoramix <sandoramixer61@gmail.com>
2022-07-09 09:24:15 +0200

    initial home page

---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import PageLayout from "../components/PageLayout";
+
+const Home: NextPage = () => {
+	return (
+		<>
+
+			<h1>Homepage</h1>
+
+		</>
+	);
+};
+
+export default Home;
