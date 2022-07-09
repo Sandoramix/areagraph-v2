aa598bce01d7dc5ae9a7b3e2a14db6093fd95b50
sandoramix <sandoramixer61@gmail.com>
2022-07-09 09:25:53 +0200

    website's page layout

---
+++ src/components/PageLayout.tsx
@@ -0,0 +1,22 @@
+import { ReactNode } from "react";
+import Header from "./Header";
+
+type PageLayoutProps = {
+	children: ReactNode,
+
+}
+
+const PageLayout = ({ children }: PageLayoutProps) => {
+	return (
+		<>
+			<Header />
+			<div className="fixed w-full h-full mt-20 bg-gray-400 dark:bg-slate-700">
+				{children}
+			</div>
+		</>
+
+	)
+}
+
+
+export default PageLayout;
