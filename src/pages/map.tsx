82f4ef39620ef4738f01bb37f5c395a6d6ea9ccc
sandoramix <sandoramixer61@gmail.com>
2022-07-11 17:37:16 +0200

    fetch instead of initialProps

--- src/pages/map.tsx
+++ src/pages/map.tsx
@@ -1,48 +1,68 @@
 import { NextPageContext } from "next";
 import { useTheme } from "next-themes";
 import dynamic from "next/dynamic";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
+import Loading from "../components/Loading";
 import { Station } from "../utils/types";
 
 import { getBaseUrl } from "./_app";
 
 
-type MapPageProps = {
-	allStations: Station[],
-	workingStations: Station[],
-}
-export default function MapPage({ allStations, workingStations }: MapPageProps) {
 
-	const { theme } = useTheme();
+export default function MapPage() {
 
+
+	const [allStations, setAllStations] = useState<Station[]>([]);
+	const [workingStations, setWorkingStations] = useState<Station[]>([]);
+	const isLoading = !allStations.length && !workingStations.length;
+
+
+	useEffect(() => {
+		const initialFetch = async () => {
+			var req = await fetch(`${getBaseUrl()}/api/stations`);
+			setAllStations(await req.json());
+
+			req = await fetch(`${getBaseUrl()}/api/stations/working`);
+			setWorkingStations(await req.json());
+		}
+		initialFetch()
+	}, [])
 
 
 
 	const Map = dynamic(() => import("../components/Map"), { ssr: false });
 
+	if (isLoading) return (
+		<>
+			<div className="h-innerpage w-innerpage relative">
+				<Loading></Loading>
+			</div>
+		</>
+	)
 
 
 	return (
 		<>
-			<div className="h-innerpage w-innerpage">
+			<div className="h-innerpage w-innerpage relative">
 
 				<Map
 
 					allStations={allStations || []}
 					workingStations={workingStations || []}
-					tilesStyle={theme === `dark` ? `jawg-dark` : `jawg-light`}
+
 				/>
+
 			</div>
 		</>
 
 	)
 }
-MapPage.getInitialProps = async (ctx: NextPageContext) => {
-	var req = await fetch(`${getBaseUrl()}/api/stations`);
-	var data = await req.json();
+// MapPage.getInitialProps = async (ctx: NextPageContext) => {
+// 	var req = await fetch(`${getBaseUrl()}/api/stations`);
+// 	var data = await req.json();
 
-	req = await fetch(`${getBaseUrl()}/api/stations/working`);
-	var w_data = await req.json();
+// 	req = await fetch(`${getBaseUrl()}/api/stations/working`);
+// 	var w_data = await req.json();
 
-	return { allStations: data, workingStations: w_data };
-}
+// 	return { allStations: data, workingStations: w_data };
+// }
