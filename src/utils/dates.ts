d33bc39720f706bead72b9d867a531bfeef1b5bd
sandoramix <sandoramixer61@gmail.com>
2022-07-13 03:16:14 +0200

    fixed potential errors

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -3,9 +3,11 @@ export function addDays(date: Date, days: number) {
 	result.setDate(result.getDate() + days);
 	return result;
 }
-export function formatDate(date: Date) {
-	return date?.toISOString().slice(0, 10) || "";
+export function formatDate(date: Date | null) {
+	if (!date) return ""
+	return date.toISOString().slice(0, 10);
 }
 export function fixDate(date: Date) {
+	if (!date) return null
 	return new Date(date.getTime() + Math.abs(date.getTimezoneOffset() * 60000))
 }
