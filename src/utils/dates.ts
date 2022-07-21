08732ec7f83b29303c4fa43957b50a2f5c1099e2
sandoramix <sandoramixer61@gmail.com>
2022-07-21 14:40:01 +0200

    updated dates.ts

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -3,10 +3,12 @@ export function addDays(date: Date, days: number) {
 	result.setDate(result.getDate() + days);
 	return result;
 }
-export function formatDate(date: Date | null) {
+export function formatDate(date: Date | null, showHours: boolean = false) {
 	if (!date) return ""
-	return date.toISOString().slice(0, 10);
+	const addPad = (s: string | number) => s.toString().padStart(2, '0')
+	return `${date.toISOString().slice(0, 10)}${showHours ? ` ${addPad(date.getHours())}:${addPad(date.getMinutes())}` : ""}`;
 }
+
 export function fixDate(date: Date) {
 	if (!date) return null
 	return new Date(date.getTime() + Math.abs(date.getTimezoneOffset() * 60000))
