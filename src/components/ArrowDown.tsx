d401a04299398be4d36da752ea901f4c32197414
sandoramix <sandoramixer61@gmail.com>
2022-07-25 00:01:46 +0200

    updated ArrowDown.tsx

--- src/components/ArrowDown.tsx
+++ src/components/ArrowDown.tsx
@@ -6,7 +6,15 @@ type ArrowDownProps = {
 const ArrowDown: FC<ArrowDownProps> = ({ className }) => {
 	return (
 		<>
-			<svg className={className || "w-full h-full"} xmlns="http://www.w3.org/2000/svg" shape-rendering="geometricPrecision" text-rendering="geometricPrecision" image-rendering="optimizeQuality" fill-rule="evenodd" clip-rule="evenodd" viewBox="0 0 512 312.36"><path fill-rule="nonzero" d="m32.66 5.71 220.49 241.11L479.35 0 512 29.87 253.12 312.36 0 35.58z" /></svg>
+			<svg className={className || "w-full h-full"}
+				xmlns="http://www.w3.org/2000/svg"
+				shapeRendering="geometricPrecision"
+				textRendering="geometricPrecision"
+				imageRendering="optimizeQuality"
+				fillRule="evenodd" clipRule="evenodd"
+				viewBox="0 0 512 312.36">
+				<path fillRule="nonzero" d="m32.66 5.71 220.49 241.11L479.35 0 512 29.87 253.12 312.36 0 35.58z" />
+			</svg>
 		</>
 	)
 }
