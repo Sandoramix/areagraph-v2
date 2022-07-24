569c388fe2001d58da33677d75187f0e0443497b
sandoramix <sandoramixer61@gmail.com>
2022-07-24 23:56:06 +0200

    updaed index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,7 +19,7 @@ const Home: NextPage = () => {
 		<>
 			<div className="h-innerpage w-innerpage select-none overflow-y-auto">
 				<div className="min-h-[180px] mb-2 w-full text-center relative text-black dark:text-white bg-inherit">
-					<div className="text-5xl font-bold font-serif min-h[60px] w-full bg-gray-400 dark:bg-zinc-900 z-10 transition-colors pt-6">AreaGraph</div>
+					<div className="text-5xl font-bold font-serif min-h[60px] w-full z-10 transition-colors pt-6">AreaGraph</div>
 					<div ref={topContent} className="w-full min-h[120px] absolute left-1/2 -translate-x-1/2 bottom-1/2 transition-all duration-700 -z-10 break-words px-4">
 						<span className="text-2xl font-semibold font-mono text-zinc-800 dark:text-gray-300">View pollutant values in the air in a graph by choosing a date range</span>
 						<br />
